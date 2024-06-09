0d3d9ff2c63e4a3842c04154f4d4264ba7397a77
srAndrii <55akif5548@gmail.com>
2024-06-08 18:17:27 -0700

    File & Folder Structure

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native";
+const AuthLayout = () => {
+    return (
+        <View>
+            <Text>AuthLayout</Text>
+        </View>
+    );
+};
+export default AuthLayout;
