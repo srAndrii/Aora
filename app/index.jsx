bcdd51e41e82e27bfa17dec815d59ffa62ad0737
srAndrii <55akif5548@gmail.com>
2024-06-08 17:09:21 -0700

    Initial setup

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import {Link} from "expo-router";
+import {StatusBar} from "expo-status-bar";
+import {StyleSheet, Text, View} from "react-native";
+
+export default function App() {
+    return (
+        <View className='flex-1 items-center justify-center bg-white'>
+            <Text className='text-3xl font-pblack'>Open up App.js to start working on your app!</Text>
+            <StatusBar style='auto' />
+            <Link href='/profile' style={{color: "blue"}}>
+                Go to profile
+            </Link>
+        </View>
+    );
+}
